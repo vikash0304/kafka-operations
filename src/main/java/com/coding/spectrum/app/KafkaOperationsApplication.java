7323250de6593a9810f62b17e7dacb341db7c4b4
vikash0304 <kr.vikash@gmail.com>
2023-10-05 10:27:03 +0530

    Basic kafka initial project

---
+++ src/main/java/com/coding/spectrum/app/KafkaOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.coding.spectrum.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaOperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding/spectrum/app/KafkaOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.spectrum.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
